90eb160c89725cefa4895ea9ec84187e355562b1
Omar Alfawareh <alfawareho@gmail.com>
2025-05-24 00:35:01 +0300

    chore: update postinstall script in package.json and add initial migration for database changes

---
+++ prisma/migrations/20250523213430_init/migration.sql
@@ -0,0 +1,46 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "Channel" (
+    "id" TEXT NOT NULL,
+    "streamerKey" TEXT NOT NULL,
+    "ingestUrl" TEXT NOT NULL,
+    "title" TEXT,
+    "description" TEXT,
+    "thumbnail" TEXT,
+    "ownerId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Channel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Channel_streamerKey_key" ON "Channel"("streamerKey");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Channel_ingestUrl_key" ON "Channel"("ingestUrl");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Channel_ownerId_key" ON "Channel"("ownerId");
+
+-- AddForeignKey
+ALTER TABLE "Channel" ADD CONSTRAINT "Channel_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
