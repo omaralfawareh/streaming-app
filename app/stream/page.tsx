029c8c093f17c4768aa459c17367f319652a9226
Omar Alfawareh <alfawareho@gmail.com>
2025-05-23 22:16:30 +0300

    ui fix

--- app/stream/page.tsx
+++ app/stream/page.tsx
@@ -2,7 +2,7 @@ import StreamBroadcast from "@/components/StreamBroadcast";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col gap-12 pt-10 px-20 h-full items-center justify-center xl:justify-start border">
+    <div className="flex flex-col gap-12 pt-10 px-7 xl:px-20 h-full items-center justify-center xl:justify-start border">
       <StreamBroadcast />
     </div>
   );
