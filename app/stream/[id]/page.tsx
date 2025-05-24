b64dbfbe3abe2e00143bb208093787ada3075f0d
Omar Alfawareh <alfawareho@gmail.com>
2025-05-24 23:47:58 +0300

    feat: enhance Stream component to fetch channel data and live status
    - Updated Stream component to retrieve channel information from the database using Prisma.
    - Implemented logic to check if the channel is live by fetching the playback URL.
    - Improved UI to display live status and playback URL in a structured format.

--- app/stream/[id]/page.tsx
+++ app/stream/[id]/page.tsx
@@ -1,5 +1,28 @@
-const Stream = ({ params: { id } }: { params: { id: string } }) => {
-  return <div>Stream {id}</div>;
+import prisma from "@/lib/prisma";
+
+const Stream = async ({ params }: { params: { id: string } }) => {
+  const { id } = await params;
+  const channel = await prisma.channel.findUnique({
+    where: { slug: id },
+  });
+  let isLive;
+
+  if (channel?.playbackUrl) {
+    isLive = await fetch(channel?.playbackUrl)
+      .then((res) => {
+        return res.ok;
+      })
+      .catch(() => {
+        return false;
+      });
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-7">
+      <h1>Live: {isLive ? "Yes" : "No"}</h1>
+      <p>Playback url: {channel?.playbackUrl}</p>
+    </div>
+  );
 };
 
 export default Stream;
